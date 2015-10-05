04568f01271c39e118a7bcf87a3f28475d3ccf3a
ivopintodasilva <ivopintodasilva@gmail.com>
2015-10-05 23:52:39 +0100

    profile serializer updated

--- core/serializers.py
+++ core/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 from core.models import Attribute, Profile
+from custom_users.serializers import CustomUserSerializer
 
 class AttributeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attribute
 
 class ProfileSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer()
+
     class Meta:
         model = Profile
